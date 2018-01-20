4bd858678e40ed3caadbb22e0aadf707bbbd046b
王希宁 <xiningwang120@gmail.com>
2018-01-19 20:47:36 -0600

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,4 @@ const mongodbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp
 mongoose.Promise = global.Promise;
 mongoose.connect(mongodbURI);
 
-module.exports = {
-    mongoose
-};
+module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectId } = require('mongodb');
@@ -8,7 +10,7 @@ var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -78,12 +80,11 @@ app.patch('/todos/:id', (req, res) => {
         body.completedAt = null;
     }
 
-    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    Todo.findByIdAndUpdate(id, body, {new: true}).then((todo) => {
         if (!todo) {
             return res.status(404).send();
         }
         res.send({todo});
-        console.log(res.body);
     }).catch(e => {
         res.status(400).send();
     });
