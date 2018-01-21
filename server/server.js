7e79d1cf0d5ccb5caf6fae42e2c167f9ee3bf3ce
王希宁 <xiningwang120@gmail.com>
2018-01-21 09:44:32 -0600

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const { ObjectId } = require('mongodb');
 const _ = require('lodash');
 
+
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
@@ -112,6 +113,19 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+// login
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then(token => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
